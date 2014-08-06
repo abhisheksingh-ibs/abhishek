d860011367f054627ea858fd77bff8e4b35c6b8c
nandanmca <nandan@iimjobs.com>
2014-08-06 13:36:13 +0530

    add includes

--- file.php
+++ file.php
@@ -1,5 +1,5 @@
 <?php
 echo '<table cell-padding="0" cell-spacing="0" border="1">
-<tr><td>test 1</td><td>test 2</td><td>test 3</td><td>test 4</td></tr>
+<tr><td>test 1</td><td>test 2</td><td>test 3</td></tr>
 </table>';
 ?>
--- includes/left.php
+++ includes/left.php
@@ -1,3 +1,3 @@
 <?php
-echo 'test on left new';
+echo 'test on left';
 ?>
